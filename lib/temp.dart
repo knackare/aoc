e8c76497a60e6cf7ca2991bd7c58aacf137c0b3d
kodknackare <4346817+knackare@users.noreply.github.com>
2024-12-01 20:45:25 +0100

    Add template

---
+++ lib/temp.dart
@@ -0,0 +1,20 @@
+﻿import 'dart:convert';
+
+int run() {
+  var ls = LineSplitter();
+  var vals = ls.convert(inp);
+
+  for (var val in vals) {
+    print(val);
+  }
+
+  var sum = 0;
+
+  return sum;
+}
+
+var ti = '''
+''';
+
+var inp = '''
+''';
